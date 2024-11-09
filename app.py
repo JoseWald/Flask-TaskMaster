ed8f6d30cdb7f363aa13d091cf9c4ecea29e9a3e
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-11-09 14:32:38 +0300

    moving the index.html in the folder templates

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import flask
+from flask import Flask , render_template
 
 #referencing this file
-app = flask(__name__)
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
